8289027d26ef57f496779ff20d4fe79b4e5f2e13
Kiril Savino <kiril@Kirils-MacBook-Pro.local>
2016-12-09 23:26:53 -0500

    Initial Commit

---
+++ Leap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leap
+//
+//  Created by Kiril Savino on 12/9/16.
+//  Copyright © 2016 Kiril Savino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
