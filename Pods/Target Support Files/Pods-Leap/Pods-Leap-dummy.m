79908d5cee2a044802c24aa912f4932c16a5e3bb
Kiril Savino <kiril.savino@gmail.com>
2016-12-11 11:55:09 -0500

    adding pods, as per CocoaPods recommendation

---
+++ Pods/Target Support Files/Pods-Leap/Pods-Leap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Leap : NSObject
+@end
+@implementation PodsDummy_Pods_Leap
+@end
---
+++ Pods/Target Support Files/Pods-LeapUITests/Pods-LeapUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeapUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LeapUITests
+@end
