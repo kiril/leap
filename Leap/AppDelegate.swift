001398bd8995b6cd6b51631bfad02c4e90fcd3e4
Kiril Savino <kiril.savino@gmail.com>
2016-12-23 23:27:18 -0500

    let's not have any warnings for now

--- Leap/AppDelegate.swift
+++ Leap/AppDelegate.swift
@@ -35,12 +35,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     func sign(_: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if (error == nil) {
             // Perform any operations on signed in user here.
+            /*
             let userId = user.userID                  // For client-side use only!
             let idToken = user.authentication.idToken // Safe to send to the server
             let fullName = user.profile.name
             let givenName = user.profile.givenName
             let familyName = user.profile.familyName
             let email = user.profile.email
+             */
             // ...
         } else {
             print("\(error.localizedDescription)")
