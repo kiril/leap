cb9335478586970cba0d1c718d95915465d7ca08
Kiril Savino <kiril.savino@gmail.com>
2016-12-23 22:19:09 -0500

    pods updated

---
+++ Leap-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Leap-Bridging-Header.h
+//  Leap
+//
+//  Created by Kiril Savino on 12/11/16.
+//  Copyright © 2016 Kiril Savino. All rights reserved.
+//
+
+#ifndef Leap_Bridging_Header_h
+#define Leap_Bridging_Header_h
+
+#import <Google/SignIn.h>
+
+
+#endif /* Leap_Bridging_Header_h */
--- Leap/AppDelegate.swift
+++ Leap/AppDelegate.swift
@@ -10,14 +10,46 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+        
+        GIDSignIn.sharedInstance().delegate = self
         return true
+    }
+    
+    func application(_ application: UIApplication,
+                     open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url,
+                                                    sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                    annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+    }
+    
+    func sign(_: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            // ...
+        } else {
+            print("\(error.localizedDescription)")
+        }
+    }
+    
+    func sign(_: GIDSignIn!, didDisconnectWith user:GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
