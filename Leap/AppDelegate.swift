20c808a4fdfb65496e2916a6636567849c767ce2
Kiril Savino <kiril.savino@gmail.com>
2017-03-14 17:07:16 -0400

    auth works

--- Leap/AppDelegate.swift
+++ Leap/AppDelegate.swift
@@ -8,16 +8,74 @@
 
 import UIKit
 import CoreData
+import Lock
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var lostCredentials: Bool = false
+    var credentials: NSManagedObject?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let context = self.persistentContainer.viewContext
+        let credentialFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Credentials")
+
+        do {
+            let credentials = try context.fetch(credentialFetch) as! [NSManagedObject]
+            if !credentials.isEmpty {
+                self.credentials = credentials[0]
+            }
+        } catch {
+            print("Failed to get credentials \(error)")
+        }
         return true
+    }
+
+    func application(_ app: UIApplication,
+                              open url: URL,
+                              options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        // com.singleleap.leap://singleleap.auth0.com/ios/com.singleleap.Leap/callback?code=lEVGl5AJl0K1a9yS&state=oCFr3Esan6AeRS136liSH3YerF1zoxwlKNtea2i6OD4
+        print("Got URL open request from \(url)")
+
+        if url.absoluteString.range(of: "auth0.com") != nil {
+            let context = self.persistentContainer.viewContext
+
+            let credentialFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Credentials")
+
+            do {
+                let credentials = try context.fetch(credentialFetch) as! [NSManagedObject]
+
+                switch credentials.count {
+                case 0:
+                    self.lostCredentials = true
+                    return Lock.resumeAuth(url, options: options)
+
+                case 1:
+                    self.lostCredentials = false
+                    self.credentials = credentials[0]
+                    if url.absoluteString.range(of: "auth0.com") != nil {
+                        return Lock.resumeAuth(url, options: options)
+                    }
+                    return true
+
+                default:
+                    self.lostCredentials = false
+                    self.credentials = credentials[0]
+                    if url.absoluteString.range(of: "auth0.com") != nil {
+                        return Lock.resumeAuth(url, options: options)
+                    }
+                    fatalError("Can't have multiple credential sets")
+                }
+            } catch {
+                print("Error finding credentials \(error)")
+                self.lostCredentials = true
+                return Lock.resumeAuth(url, options: options)
+            }
+        }
+
+        return false
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -58,7 +116,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -90,4 +148,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
--- Leap/ViewController.swift
+++ Leap/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Lock
+import SafariServices
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -22,23 +24,40 @@ class ViewController: UIViewController {
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        Lock
-            .classic()
-            .withStyle {
-                $0.title = ""
-                $0.primaryColor = .magenta
-                $0.logo = LazyImage(name: "Logo Small Clear")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+        if let credentials = appDelegate.credentials {
+            print("holy shit we're in!")
+        } else {
+            if appDelegate.lostCredentials {
+                let logoutURL = "com.singleleap.Leap://singleleap.auth0.com/ios/com.singleleap.Leap/logout".addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+                let url = "https://singleleap.auth0.com/v2/logout?returnTo=\(logoutURL)"
+                let svc = SFSafariViewController(url: URL(string: url)!)
+                self.present(svc, animated: true, completion: nil)
+
+            } else {
+
+                print("presenting login")
+                Lock
+                  .classic()
+                  .withStyle {
+                      $0.title = ""
+                      $0.primaryColor = .magenta
+                      $0.headerBlur = .extraLight
+                      $0.logo = LazyImage(name: "Logo Small Clear")
+                  }
+                  .withOptions {
+                      $0.oidcConformant = true
+                  }
+                  .onAuth { credentials in
+                      print("well hey, here we are!")
+                      let creds = NSEntityDescription.insertNewObject(forEntityName: "Credentials", into: appDelegate.persistentContainer.viewContext)
+                      creds.setValue(credentials.accessToken, forKey: "accessToken")
+                      creds.setValue(credentials.idToken, forKey: "idToken")
+                      try! appDelegate.persistentContainer.viewContext.save()
+                  }
+                  .present(from: self)
             }
-            .withOptions {
-                $0.oidcConformant = true
-            }
-            .onAuth { credentials in
-                // Do something with credentials e.g.: save them.
-                // Lock will not save these objects for you.
-                // Lock will dismiss itself automatically by default.
-            }
-            .present(from: self)
+        }
     }
-
-
 }
