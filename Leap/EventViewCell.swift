9681ab9de67cd4facd4d847cfde2a7ebbd30e4f4
Chris Ricca <christopher.ricca@gmail.com>
2017-03-16 15:53:21 -0400

    Migrated to using collection view for local calendars

---
+++ Leap/EventViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EventViewCell.swift
+//  Leap
+//
+//  Created by Chris Ricca on 3/16/17.
+//  Copyright © 2017 Kiril Savino. All rights reserved.
+//
+
+import UIKit
+
+class EventViewCell: UICollectionViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+}
