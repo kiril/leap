e35dfa5414faa719a202237b0113de54896da1bb
Kiril Savino <kiril.savino@gmail.com>
2017-03-27 16:58:15 -0400

    more tests that pass

--- LeapTests/EventKitMigrationTests.swift
+++ LeapTests/EventKitMigrationTests.swift
@@ -27,11 +27,42 @@ class EventKitMigrationTests: XCTestCase {
 
     func testRecurrence() {
         let ek1 = EKRecurrenceRule(recurrenceWith: EKRecurrenceFrequency.daily,
-                                 interval: 1, end: EKRecurrenceEnd(occurrenceCount: 2))
+                                   interval: 1, end: EKRecurrenceEnd(occurrenceCount: 2))
         let r1: Recurrence = ek1.asRecurrence()
         XCTAssertEqual(r1.count, 2)
         XCTAssertEqual(r1.interval, 1)
         XCTAssertEqual(r1.frequency, Frequency.daily)
+
+        let ek2 = EKRecurrenceRule(recurrenceWith: EKRecurrenceFrequency.weekly,
+                                   interval: 2, end: EKRecurrenceEnd(occurrenceCount: 1))
+        let r2: Recurrence = ek2.asRecurrence()
+        XCTAssertEqual(r2.count, 1)
+        XCTAssertEqual(r2.interval, 2)
+        XCTAssertEqual(r2.frequency, Frequency.weekly)
+
+        let ek3 = EKRecurrenceRule(recurrenceWith: EKRecurrenceFrequency.yearly, interval: 2, daysOfTheWeek: [EKRecurrenceDayOfWeek(EKWeekday.tuesday)], daysOfTheMonth: nil, monthsOfTheYear: nil, weeksOfTheYear: [1 as NSNumber, -2 as NSNumber], daysOfTheYear: nil, setPositions: nil, end: nil)
+        let r3 = ek3.asRecurrence()
+
+        XCTAssertEqual(r3.count, 0)
+        XCTAssertEqual(r3.interval, 2)
+        XCTAssertEqual(r3.frequency, Frequency.yearly)
+        XCTAssertEqual(r3.daysOfWeek.count, 1)
+        XCTAssertEqual(r3.daysOfWeek[0].dayOfWeek, DayOfWeek.tuesday)
+
+        let endDate = Calendar.current.date(byAdding: DateComponents(year: 1), to: Date())!
+        let ek4 = EKRecurrenceRule(recurrenceWith: EKRecurrenceFrequency.weekly, interval: 1, daysOfTheWeek: [EKRecurrenceDayOfWeek(EKWeekday.tuesday), EKRecurrenceDayOfWeek(EKWeekday.thursday)], daysOfTheMonth: nil, monthsOfTheYear: [1 as NSNumber, 2 as NSNumber], weeksOfTheYear: nil, daysOfTheYear: nil, setPositions: nil, end: EKRecurrenceEnd(end: endDate))
+        let r4 = ek4.asRecurrence()
+
+        XCTAssertEqual(r4.count, 0)
+        XCTAssertEqual(r4.interval, 1)
+        XCTAssertEqual(r4.frequency, Frequency.weekly)
+        XCTAssertEqual(r4.daysOfWeek.count, 2)
+        XCTAssertEqual(r4.daysOfWeek[0].dayOfWeek, DayOfWeek.tuesday)
+        XCTAssertEqual(r4.daysOfWeek[1].dayOfWeek, DayOfWeek.thursday)
+        XCTAssertEqual(r4.monthsOfYear.count, 2)
+        XCTAssertEqual(r4.monthsOfYear[0].value, 1)
+        XCTAssertEqual(r4.monthsOfYear[1].value, 2)
+        XCTAssertEqual(Int(r4.endTime!.timeIntervalSince1970), Int(endDate.timeIntervalSince1970)) // weird that comparing dates directly doesn't work
     }
     
     func testExample() {
