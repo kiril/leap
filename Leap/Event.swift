ce1aa672e72c9cafdf6d66cf19b824f36ab6cc28
Kiril Savino <kiril.savino@gmail.com>
2017-04-05 15:35:46 -0400

    ok, so that's deprecated, apparently

--- Leap/Event.swift
+++ Leap/Event.swift
@@ -59,7 +59,7 @@ class Event: _TemporalBase, Temporality {
 
     static func between(_ starting: Date, and before: Date) -> Results<Event> {
         let predicate = NSPredicate(format: "(startTime >= %d AND startTime < %d) OR (endTime >= %d AND endTime <= %d)", starting.secondsSinceReferenceDate, before.secondsSinceReferenceDate, starting.secondsSinceReferenceDate, before.secondsSinceReferenceDate)
-        return Realm.user().objects(Event.self).filter(predicate).sorted(byProperty: "startTime")
+        return Realm.user().objects(Event.self).filter(predicate).sorted(byKeyPath: "startTime")
         // TODO: also spanning this range??? (notes???)
     }
 }
