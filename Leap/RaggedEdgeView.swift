8e3cf1914f5c807cd33cd5bc7dc1a483a7778867
Kiril Savino <kiril.savino@gmail.com>
2017-05-05 12:24:25 -0400

    WIP on jagged edge

---
+++ Leap/RaggedEdgeView.swift
@@ -0,0 +1,15 @@
+//
+//  RaggedEdgeView.swift
+//  Leap
+//
+//  Created by Kiril Savino on 5/5/17.
+//  Copyright © 2017 Single Leap, Inc. All rights reserved.
+//
+
+import UIKit
+
+class RaggedEdgeView: UIView {
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+}
